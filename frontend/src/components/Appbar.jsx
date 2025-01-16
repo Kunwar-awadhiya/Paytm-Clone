27e560033134cd921fa801fc25f340bf0a911edb
Kunwar Awadhiya <kunwarawadhiya@gmail.com>
2025-01-16 22:09:36 +0530

    Update Appbar.jsx

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,32 +1,3 @@
-/*
-import React from 'react'
-
-const Appbar = () => {
-  return (
-    <div className='shadow h-12 flex justify-between'>
-        <div className='flex flex-1 justify-center h-full ml-4 '>
-            Paytm App
-        </div>
-        <div className='flex'>
-            <div className='flex flex-col justify-center h-full mr-4'>
-                hello
-            </div>
-            <div className='rounded-full h-12 m-12 bg-slate-200 flex justify-center'>
-                <div className='flex flex-col justify-center h-full text-center'>
-                    U
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-  )
-}
-
-export default Appbar;
-*/
-
-
 import React from 'react';
 
 const Appbar = () => {
